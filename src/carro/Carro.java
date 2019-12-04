6c9a22de9dadd3ea1cfc1eb597a30c521ca63a0c
Visitante7 <Visitante7@74327-U.asav.brm>
2019-12-04 17:45:10 -0200

    Adicionado primeira árvore de código.

---
+++ src/carro/Carro.java
@@ -0,0 +1,13 @@
+
+package carro;
+
+public class Carro {
+
+    public static void main(String[] args) {
+         Carro1 c1 = new Carro1();
+         c1.cor= "verde";
+         c1.numdeportas= 4;
+         c1.Detalhes();
+    }
+    
+}
---
+++ src/carro/Carro1.java
@@ -0,0 +1,14 @@
+
+package carro;
+
+public class Carro1 {
+   String cor;
+   int numdeportas;
+   
+   
+    public void Detalhes(){
+        System.out.println("cor " + cor);
+        System.out.println("numero de portas " + numdeportas);
+    
+    }
+}
